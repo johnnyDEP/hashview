0479a50c29453ec2d434fc430abe23a7ff607f5c
i128 <jarsnah12@gmail.com>
2024-08-04 18:12:46 +0000

    Added ability to edit existing rules file

--- hashview/rules/routes.py
+++ hashview/rules/routes.py
@@ -1,9 +1,9 @@
 """Flask routes to handle Rules"""
 import os
-from flask import Blueprint, render_template, flash, url_for, redirect, current_app
+from flask import Blueprint, render_template, flash, url_for, redirect, current_app, request
 from flask_login import login_required, current_user
 from hashview.models import Rules, Tasks, Jobs, JobTasks, Users
-from hashview.rules.forms import RulesForm
+from hashview.rules.forms import RulesForm, RulesEditForm
 from hashview.utils.utils import save_file, get_linecount, get_filehash
 from hashview.models import db
 
@@ -44,6 +44,56 @@ def rules_add():
             flash('Rules File created!', 'success')
             return redirect(url_for('rules.rules_list'))
     return render_template('rules_add.html', title='Rules Add', form=form)
+
+@rules.route("/rules/edit/<int:rule_id>", methods=['GET', 'POST'])
+@login_required
+def rules_edit(rule_id):
+
+    rules = Rules.query.get(rule_id)
+    if not rules:
+        flash('Invalid Rule File')
+        return redirect(url_for('rules.rules_list'))
+
+    if current_user.admin or rules.owner_id == current_user.id:
+
+        form = RulesEditForm()
+        if request.method == 'GET':
+            with open(rules.path, "r") as file:
+                S = file.read()
+
+            form.name.data = rules.name
+            form.content.data = S
+
+
+        elif request.method == 'POST':
+
+            if not form.name.data:
+                flash('Rules file requires a name.')
+                return redirect(url_for('rules.rules_edit'))
+            if not form.content.data:
+                flash('Rule data must be populated')
+                return redirect(url_for(rules.rules_edit()))
+
+            # remove old rules file
+            os.remove(rules.path)
+
+            # create new file
+            rules_file = open(rules.path, 'w+')
+            rules_file.write(form.content.data)
+            rules_file.close()
+
+            rules.name = form.name.data
+            rules.size = get_linecount(rules.path)
+            rules.checksum = get_filehash(rules.path)
+            db.session.commit()
+
+            flash('Rules updated', 'success')
+        
+    
+        return render_template('rules_edit.html', title='Rules Edit', form=form, rules_name = rules.name)
+    else:   
+        flash('Unauthorzed Action!', 'danger')
+        return redirect(url_for('rules.rules_list'))
 
 @rules.route("/rules/delete/<int:rule_id>", methods=['GET', 'POST'])
 @login_required
